06bf195b9f2bf26f4913f4fab851fe39cedef411
ducthang2802 <72588250+ducthang2802@users.noreply.github.com>
2021-09-28 16:00:45 +0700

    Add files via upload

---
+++ Bai2.java
@@ -0,0 +1,28 @@
+package Tuan1;
+
+import java.util.Scanner;
+
+public class Bai2 {
+	 public static void main(String args[])
+	    {
+	        int temp;
+	        boolean isPrime=true;
+	        Scanner scan= new Scanner(System.in);
+	        System.out.println("Nhập vào số cần kiểm tra:");
+	        int num=scan.nextInt();
+	        scan.close();
+	        for(int i=2;i<=num/2;i++)
+	        {
+	            temp=num%i;
+	            if(temp==0)
+	            {
+	                isPrime=false;
+	                break;
+	            }
+	        }
+	        if(isPrime)
+	            System.out.println(num + " Là số nguyên tố!");
+	        else
+	            System.out.println(num + " Không phải là số nguyên tố!");
+	    }
+}
